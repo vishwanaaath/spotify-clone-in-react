951393b7285d92d706869250ae9d343356de1b05
vishwanaaath <vishwanathgowda2022@gmail.com>
2025-02-28 21:38:45 +0530

    search work well

--- src/App.jsx
+++ src/App.jsx
@@ -276,16 +276,7 @@ const RowCard = ({ playlist }) => {
       <div className="h-[64px] flex-1 flex flex-col justify-center p-2 ml-1  md:flex">
         <p className="text-[15px]">{playlist.title}</p>
         <p className="text-[14px] text-[#A5A5A5]">{playlist.description}</p>
-        <button >
-              <svg
-                data-encore-id="icon"
-                role="img"
-                aria-hidden="true"
-                class="e-9640-icon"
-                viewBox="0 0 24 24">
-                <path d="m7.05 3.606 13.49 7.788a.7.7 0 0 1 0 1.212L7.05 20.394A.7.7 0 0 1 6 19.788V4.212a.7.7 0 0 1 1.05-.606z"></path>
-              </svg>
-        </button>
+        
       </div>
     </div>
   );
